af52cf69978b9107a740d1996b3586f2c9965459
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2023-04-28 15:54:42 +0900

    Add files via upload

---
+++ vision/datatrans.py
@@ -0,0 +1,44 @@
+import pymysql, logging, sys
+import dataprocess
+
+host = "mysql.hostname.com"
+port = "3306"
+database = "result"       # 우리가 쓰는 데이터베이스 이름
+username = "sunny"      # 우리가 쓰는 유저네임
+password = "sunny123"   # 우리가 쓰는 패스워드
+
+
+def main():
+
+    try:
+    #DB Connection 생성
+        conn = pymysql.connect(host, user=username, passwd=password, db=database, use_unicode=True, charset='utf8')
+        cursor = conn.cursor()
+
+    except:
+        logging.error("")
+        sys.exit(1)
+
+	
+    # cursor.execute("show tables")
+    #print(cursor.fetchall())
+
+    # 무ㅓ넣을까
+    # 일단 DB 새로 생성해놓고 (result db)
+    # result, datetime, temp, humadity? 넘겨야 하나
+    # 아니면 result fk fk ? 이건 너무 복잡할듯..
+    query = """INSERT INTO result(result, date_time, temp, humadity)
+                VALUES (%s, %s, %.1f, %.1f)"""
+    result = dataprocess.result
+    date_time = dataprocess.date_time
+    temp = dataprocess.temp
+    humadity = dataprocess.humadity
+
+    cursor.execute(query, (result, date_time, temp, humadity))
+    conn.commit()
+    
+    conn.close()
+    
+    
+if __name__ == '__main__':
+	main()
