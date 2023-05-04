531aad7b177c607810345ed488a361d4c0c8146e
epsin7783 <62544043+epsin7783@users.noreply.github.com>
2023-05-04 19:30:00 +0900

    Delete user directory

--- user/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from MySQLdb.constants.FLAG import AUTO_INCREMENT
-
-class User(models.Model):
-    id = models.CharField(max_length=30, verbose_name="아이디", primary_key=True)
-    passwd = models.CharField(max_length=30, verbose_name="비밀번호", null=False)
--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls.conf import path
-from user import views
-
-app_name = "user"
-urlpatterns = [
-        path( "user", views.User.as_view(), name="user" )  
-    ]
