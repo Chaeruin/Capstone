0d08bb66a649d3f9c08671e0bcc11e08571358df
espin7783 <espin7783@nate.com>
2023-05-04 19:28:12 +0900

    delete unnecessary files

--- user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from user.models import User
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("no", "id", "name", "nickname", "pw", "email", "phone")
-admin.site.register(User, UserAdmin)
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls.conf import path
-from user import views
-
-app_name = "user"
-urlpatterns = [
-        path( "user", views.User.as_view(), name="user" ),
-        path("login",views.Login.as_view(), name="login"),
-        path("signin",views.Signin.as_view(), name="signin")
-    ]
