7941f3cdb26c9bd4e79876e353a911c4d9c13671
Chaeruin <cms3604@naver.com>
2023-05-05 09:54:16 +0900

    modify commit

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,5 @@ from main import views
 
 app_name = "main"
 urlpatterns = [
-        path( "main", views.Main.as_view(), name="main" ), 
-        path( "login", views.Login.as_view(), name="login" ) , 
-        path( "signup", views.Signup.as_view(), name="signup" ),
-        path( "scandata", views.Scandata.as_view(), name="scandata" ),
-        path( "datainner", views.Datainner.as_view(), name="datainner" ),    
+        path( "main", views.Main.as_view(), name="main" )  
     ]
--- main/views.py
+++ main/views.py
@@ -2,23 +2,9 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from django.template import loader
 from django.http.response import HttpResponse, JsonResponse
-import imageread
-from datetime import datetime
-from ..rspi import testDHT22 as dht
-import pymysql, logging, sys
-import dataprocess
-import io
-import os
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service_secret_key.json'
 
 
-# Imports the Google Cloud client library
-from google.cloud import vision 
-from google.cloud.vision_v1 import types
-
-
-class Main(View):   #main
+class Main(View):
     def get(self, request):
         template = loader.get_template("base.html")
         context = {        }
@@ -26,79 +12,5 @@ class Main(View):   #main
 
     def post(self, request):
         template = loader.get_template("base.html")
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-    
-class Login(View):  #로그인
-    def get(self, request):
-        template = loader.get_template("login.html")
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-
-    def post(self, request):
-        template = loader.get_template("login.html")
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-
-class Signup(View): #회원가입
-    def get(self, request):
-        template = loader.get_template("signup.html")
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-
-    def post(self, request):
-        template = loader.get_template("signup.html")
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-
-class Scandata(View):
-    def get(self, request):
-        template = loader.get_template("scandata.html")
-        ident = imageread.texts[0].description
-
-        # Decide the result of ident
-        if ident == '11' or 'II':
-            result = '양성'
-        elif ident == '1' or 'I':
-            result = '음성'
-        else:
-            result = '검출 오류'
-
-        # Take real date time
-        date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        # id, humidity and temp
-
-        # id = 0                        # auto increament, 자동 생성으로 바꾸기
-        humidity = dht.humidity         # rsp 바로 연동해서 그 값 가져오기
-        temp = dht.temperature          # rsp 바로 연동해서 그 값 가져오기
-    
-
-        
-        # cursor.execute("show tables")
-        #print(cursor.fetchall())
-
-        # 무ㅓ넣을까
-        # 일단 DB 새로 생성해놓고 (result db)
-        # result, datetime, temp, humidity? 넘겨야 하나
-        # 아니면 result fk fk ? 이건 너무 복잡할듯..
-        query = """INSERT INTO result(result, date_time, temp, humidity)
-                    VALUES (%s, %s, %.1f, %.1f)"""
-        
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-
-    def post(self, request):
-        template = loader.get_template("scandata.html")
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-
-class Datainner(View):
-    def get(self, request):
-        template = loader.get_template("datainner.html")
-        context = {        }
-        return HttpResponse(template.render( context, request ))
-
-    def post(self, request):
-        template = loader.get_template("datainner.html")
         context = {        }
         return HttpResponse(template.render( context, request ))
